168442e5213d4d18d53c3cfc8eba93eafa1a893c
Leonardo Peter <leopeter98@hotmail.com>
2024-01-10 22:20:39 -0300

    Servidor Express

---
+++ src/ProductsManager.js
@@ -0,0 +1,91 @@
+import {promises as fs} from "fs"
+
+export class ProductManager {
+    constructor(){
+        this.patch = "./productos.txt"
+        this.products = []
+    }
+
+    static id = 0
+
+    addProduct = async (title, description, price, img, code, stock) => {
+        ProductManager.id++;
+        let newProduct = {
+            title,
+            description,
+            price,
+            img,
+            code,
+            stock,
+            id: ProductManager.id
+        };
+
+        this.products.push(newProduct)
+
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+    };
+
+    readProducts = async () => {
+       let respuesta = await fs.readFile(this.patch, "utf-8")
+        return JSON.parse(respuesta)
+    }
+
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts();
+        return console.log(respuesta2)
+    }
+
+    getProductsById = async (id) => {
+        let respuesta3 = await this.readProducts();
+        id = parseInt(id);
+        if(!respuesta3.find((product) => product.id === id)){
+            console.log("Producto no encontrado")
+        }else{
+            console.log(respuesta3.find((product) => product.id === id));
+        }
+    };
+
+    deleteProductsById = async (id) => {
+            let respuesta3 = await this.readProducts();
+            let productfilter = respuesta3.filter(products => products.id != id);
+            await fs.writeFile(this.patch, JSON.stringify(productfilter));
+            console.log("Producto eliminado");
+    };
+
+    updateProduct = async ({id, ...updatedFields}) => {
+        let products = await this.readProducts();
+        const index = products.findIndex(product => product.id === id);
+    
+        if (index !== -1) {
+            products[index] = { ...products[index], ...updatedFields };
+            await fs.writeFile(this.patch, JSON.stringify(products));
+            console.log("Producto actualizado");
+        } else {
+            console.log("Producto no encontrado");
+        }
+    };
+}
+
+// const productos = new ProductManager();
+
+// productos.addProduct("Remera", "overside negra", 25000, "img1", "asd123", 18);
+// productos.addProduct("Pantalon", "Cargo verde", 35000, "img2", "asd456", 15);
+// productos.addProduct("Remera", "overside blanca", 26000, "img3", "asd678",10);
+// productos.addProduct("Buzo", "overside negro", 45000, "img4", "asd678",17);
+// productos.addProduct("Remera", "overside gris", 26000, "img5", "asd678",16);
+// productos.addProduct("Buzo", "overside gris", 42000, "img6", "asd678",18);
+// productos.addProduct("Musculosa", "overside negra", 25000, "img7", "asd678",15);
+// productos.addProduct("Pantalon", "cargo arena", 35000, "img8", "asd678",13);
+
+// productos.getProducts()
+
+//  productos.getProductsById(4)
+
+//  productos.deleteProductsById(1)
+
+// productos.updateProduct({
+//   price: 26000,
+//   stock: 15,
+//   id:1
+  
+// })
---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import { ProductManager } from "./ProductsManager.js"
+
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+
+const productos = new ProductManager();
+const readProducts = productos.readProducts();
+
+app.get("/products", async (req, res) => {
+    let limit = parseInt(req.query.limit);
+    if(!limit) return res.send(await readProducts);
+    let allProducts = await readProducts;
+    let productLimit = allProducts.slice(0, limit);
+    res.send(productLimit);
+});
+
+app.get("/products/:id", async (req, res) => {
+    let id = parseInt(req.params.id);
+    let allProducts = await readProducts;
+    let productById = allProducts.find(product => product.id === id);
+    res.send(productById);
+});
+
+app.listen(3000, () => {
+console.log("aplicacion en el puerto 3000")
+});
